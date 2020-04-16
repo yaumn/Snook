b73d6565806a6a5e96b16414beb73d5734a39b4a
Guillaume André <guillaume.andre33@gmail.com>
2020-04-16 13:51:45 +0200

    Add file logging. Add autocompletion of download and upload. Ctrl-c do not quit snook anymore

--- snook_listener.py
+++ snook_listener.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 
 
-import argparse
+from argparse import ArgumentParser
 from base64 import b64decode, b64encode
 from cmd import Cmd
+from glob import glob
 from json import dumps, JSONDecodeError, loads
 import logging
 from os.path import abspath, getsize, isdir, join
 from ntpath import basename
-import socket
-import sys
+from readline import get_completer_delims, set_completer_delims
+from shlex import split
+from socket import AF_INET, SHUT_RDWR, socket, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 
 from alive_progress import alive_bar
 from colorama import Fore, init
+
+
+# Little trick to enable correct path autocompletion on Unix
+old_delims = get_completer_delims()
+set_completer_delims(old_delims.replace('/', ''))
+
+logger = logging.getLogger('Snook')
+
+
+class SilentArgumentParser(ArgumentParser):
+
+    def error(message):
+        self.exit(2)
 
 
 class Packet():
@@ -57,21 +72,67 @@ class Prompt(Cmd):
         self.sock = client_sock
         self.receive_loop()
 
+    def cmdloop(self, intro=None):
+        while True:
+            try:
+                super().cmdloop(intro=intro)
+                break
+            except KeyboardInterrupt:
+                print('^C')
+
+    def complete_download(self, text, line, begidx, endidx):
+        parser = SilentArgumentParser()
+        parser.add_argument('file', nargs='?', type=str)
+        parser.add_argument('-d', '--destination', type=str)
+
+        try:
+            args = parser.parse_args(self.split_line_for_completion(line[8:]))
+        except SystemExit:
+            return
+
+        if args.destination and line.index(args.destination) != begidx:
+            return
+
+        path_to_complete = args.destination[:endidx - begidx]
+        paths = glob(path_to_complete + '*')
+
+        return paths
+
+    def complete_upload(self, text, line, begidx, endidx):
+        parser = SilentArgumentParser()
+        parser.add_argument('file', type=str)
+        parser.add_argument('-d', '--destination', type=str)
+
+        try:
+            args = parser.parse_args(self.split_line_for_completion(line[6:]))
+        except SystemExit:
+            return
+
+        if args.file and line.index(args.file) != begidx:
+            return
+
+        path_to_complete = args.file[:endidx - begidx]
+        paths = glob(path_to_complete + '*')
+
+        return paths
+
     def do_download(self, line):
-        parser = argparse.ArgumentParser(prog='download',
+        parser = ArgumentParser(prog='download',
             description='Download file from remote host')
         parser.add_argument('file', type=str,
             help='Path to the file on the remote host')
         parser.add_argument('-d', '--destination', type=str, default='.',
             help='Destination of the downloaded file')
         try:
-            args = parser.parse_args(line.split(' '))
+            args = parser.parse_args(split(line))
         except SystemExit:
             return
 
         if not line:
             parser.print_usage()
             return
+
+        logger.debug('Running command: download {}'.format(line))
 
         self.task_args = args
 
@@ -81,20 +142,24 @@ class Prompt(Cmd):
         self.send_packet(p)
         return self.receive_loop()
 
+    def do_EOF(self, line):
+        return self.do_exit()
+
     def do_exit(self, line=''):
-        self.sock.shutdown(socket.SHUT_RDWR)
+        logger.debug('Running command: {}'.format(line))
+        self.sock.shutdown(SHUT_RDWR)
         self.sock.close()
         return True
 
     def do_upload(self, line):
-        parser = argparse.ArgumentParser(prog='upload',
+        parser = ArgumentParser(prog='upload',
             description='Upload file to remote host')
         parser.add_argument('file', type=str,
             help='Path to the file to upload')
         parser.add_argument('-d', '--destination', type=str, default='.',
             help='Destination of the uploaded file')
         try:
-            args = parser.parse_args(line.split(' '))
+            args = parser.parse_args(split(line))
         except SystemExit:
             return
 
@@ -113,12 +178,15 @@ class Prompt(Cmd):
             self.print_error(str(e))
             return
 
+        logger.debug('Running command: upload {}'.format(line))
+
         p.add_argument('dest', args.destination)
         p.add_argument('filename', basename(args.file))
         self.send_packet(p)
         return self.receive_loop()
 
     def default(self, line):
+        logger.debug('Running command: {}'.format(line))
         p = Packet()
         p.add_argument('cmd', b64encode(line.encode('utf-8')).decode('utf-8'))
         self.send_packet(p)
@@ -183,19 +251,14 @@ class Prompt(Cmd):
 
         self.print_message('File successfully uploaded to {}'.format(p.message))
 
-    @staticmethod
-    def print_color(text, color):
-        end = '' if text.endswith('\n') else '\n'
-        print(color + text + Fore.RESET, end=end, flush=True)
-
     def print_error(self, text):
-        self.print_color(text, Fore.RED)
+        logger.error(Fore.RED + text + Fore.RESET)
 
     def print_message(self, text):
-        self.print_color(text, Fore.RESET)
+        logger.info(text)
 
     def print_warning(self, text):
-        self.print_color(text, Fore.YELLOW)
+        logger.warning(Fore.YELLOW + text + Fore.RESET)
 
     def receive_json_packet(self):
         # TODO: Find a better mechanism for this function that can
@@ -212,7 +275,7 @@ class Prompt(Cmd):
             except JSONDecodeError as e:
                 continue
             except Exception as e:
-                print('An unexcepted error happened:', e)
+                logger.error('An unexcepted error happened:', e)
                 return None
 
             break
@@ -236,32 +299,63 @@ class Prompt(Cmd):
     def send_packet(self, packet):
         self.sock.send(packet.dumps().encode('utf-8'))
 
+    def split_line_for_completion(self, line):
+        """ 
+        Split a command line like a shell would do but still keep
+        empty arguments (i.e. two spaces in a row) to enable autocompletion       
+        """
+        if not line.endswith('  '):
+            line += ' '
+        return split(line.replace('  ', ' "" '))
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Listen for incoming connections')
+    parser = ArgumentParser(description='Listen for incoming connections')
     parser.add_argument('-H', '--host', type=str, default='0.0.0.0',
         help='Local hostname or IP address to bind to (default: 0.0.0.0)')
     parser.add_argument('-p', '--port', type=int, default=1337,
         help='Local port to bind to (default: 1337)')
+    parser.add_argument('-l', '--log', nargs='?', type=str,
+        help='Path to the file where to log commands and outputs')
     args = parser.parse_args()
 
-    init()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_address = (args.host, args.port)
-    sock.bind(server_address)
+    # Setup logger
+    logger.setLevel(logging.DEBUG)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.INFO)
+    consoleFormatter = logging.Formatter('%(message)s')
+    consoleHandler.setFormatter(consoleFormatter)
+    logger.addHandler(consoleHandler)
+
+    if args.log:
+        fileHandler = logging.FileHandler(args.log)
+        fileHandler.setLevel(logging.DEBUG)
+        fileFormatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+        fileHandler.setFormatter(fileFormatter)
+        logger.addHandler(fileHandler)
+
+    init()  # colorama
+
+    sock = socket(AF_INET, SOCK_STREAM)
 
     try:
+        # Setup socket
+        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        server_address = (args.host, args.port)
+        sock.bind(server_address)
         sock.listen()
-        print('Listening on {}:{}'.format(*server_address))
+        logger.info('Listening on {}:{}'.format(*server_address))
         client_socket, client_address = sock.accept()
-        print('Received connection from {}:{}\n'.format(*client_address))
+
+        logger.info('Received connection from {}:{}\n'.format(*client_address))
         prompt = Prompt(client_socket)
         prompt.cmdloop()
     except Exception as e:
-        client_socket.shutdown(socket.SHUT_RDWR)
+        logger.exception(e)
+        client_socket.shutdown(SHUT_RDWR)
         client_socket.close()
         raise
     finally:
-        sock.shutdown(socket.SHUT_RDWR)
+        sock.shutdown(SHUT_RDWR)
         sock.close()
